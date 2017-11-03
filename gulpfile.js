870f0a0c239032936bfe77dbedc9d12b98ebd1ba
Will Arends <willarends@gmail.com>
2017-11-02 21:59:29 -0700

    init

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const gulp = require('gulp'),
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	//babel = require('gulp-babel'),
+	gutil = require('gulp-util');
+    path = './wp-content/themes/ActuCall';
+
+const paths = {
+	//js: ['js/modules/*.js', 'js/*.js'],
+	css: [path + '/sass/**/*.scss'],
+	//img: ['img/*.svg', 'img/*.png', 'img/*.jpg'],
+	vendor: ['vendor/jquery.history.js',
+			'vendor/jquery.js',
+			'vendor/TweenMax.min.js',
+			'vendor/MorphSVGPlugin.min.js',
+			'vendor/DrawSVGPlugin.min.js',
+			'vendor/slick.min.js']
+}
+
+gulp.task('sass', function() {
+  return gulp.src(path + '/sass/styles.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest(path));
+});
+
+// gulp.task('js', function(){
+// 	gulp.src(paths.js)
+// 	.pipe(sourcemaps.init())
+// 		.pipe(babel({ presets: ['es2015'] }))
+// 		.on('error', gutil.log)
+// 		.pipe(concat('app.js'))
+// 	.pipe(sourcemaps.write())
+// 	.pipe(gulp.dest('dist/www-content/web-apps/indigo-slate/js'))
+// 	.pipe(connect.reload());
+// });
+
+gulp.task('watch', function() {
+  	//gulp.watch(paths.js, ['js']);
+  	gulp.watch(paths.css, ['sass']);
+});
+
+
+gulp.task('vendor', function(){
+	gulp.src(paths.vendor)
+		.pipe(gulp.dest('dist/www-content/web-apps/indigo-slate/vendor'))
+});
+
+gulp.task('default', ['watch']);
+//gulp.task('build', ['templates','vendor']);
---
+++ wp-content/themes/ActuCall/page-home.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	Template Name: Home Page
+*/
+
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<img class="img-responsive" src="<?php bloginfo('stylesheet_directory'); ?>/assets/img/coming-soon.png" alt="Elevated Foresight">
+
+
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+
+get_footer();
